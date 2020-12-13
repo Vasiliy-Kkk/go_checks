b71493c5d4d190a0c98e64bf21a3b176c813cb16
vasiliykadikov <vasiliykadikov@inbox.ru>
2020-12-13 20:27:47 +0300

    delete files

--- tools/polly/examples/04-framework/out/build/x64-Debug (по умолчанию)/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Василий/source/repos/vasiliykadikov/go_game/tools/polly/examples/04-framework
-# Build directory: C:/Users/Василий/source/repos/vasiliykadikov/go_game/tools/polly/examples/04-framework/out/build/x64-Debug (по умолчанию)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(SimpleTest "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe" "-E" "echo" "Skip test")
-set_tests_properties(SimpleTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Василий/source/repos/vasiliykadikov/go_game/tools/polly/examples/04-framework/CMakeLists.txt;20;add_test;C:/Users/Василий/source/repos/vasiliykadikov/go_game/tools/polly/examples/04-framework/CMakeLists.txt;0;")
