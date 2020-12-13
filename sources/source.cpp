4a59967bc378c6b76c0b24ff0847f443028afe13
vasiliykadikov <vasiliykadikov@inbox.ru>
2020-12-13 20:10:50 +0300

    update

--- sources/source.cpp
+++ sources/source.cpp
@@ -8,10 +8,10 @@ int main()
 				// in windows:
 				//int scrX = GetSystemMetrics(SM_CXSCREEN); //îïòèìàëüíûå ïàðàìåòðû îêíà 
 				//int scrY = GetSystemMetrics(SM_CYSCREEN);
-				int scrX = 1600;
-				int scrY = 900;
-				sf::RenderWindow window(sf::VideoMode(scrX, scrY), "Go", sf::Style::Close);
-				window.setVerticalSyncEnabled(true); //âåðòèêàëüíàÿ ñèíõðîíèçàöèÿ îêíà
+		//		int scrX = 1600;
+		//		int scrY = 900;
+		//		sf::RenderWindow window(sf::VideoMode(scrX, scrY), "Go", sf::Style::Close);
+		//		window.setVerticalSyncEnabled(true); //âåðòèêàëüíàÿ ñèíõðîíèçàöèÿ îêíà
 		//		MainMenu menu(scrX, scrY);
 			//	menu.print_window(window);
 				return 0;
